79f0d03d254e730fb96acc18652587ec7644a04e
Mauricio Laine <laine.mau@gmail.com>
2020-01-19 15:24:37 -0600

    set two buttons to show the different dialogues, and made code clean up

--- app/src/main/java/com/laine/mauro/datepickertest/MainActivity.kt
+++ app/src/main/java/com/laine/mauro/datepickertest/MainActivity.kt
@@ -3,7 +3,9 @@ package com.laine.mauro.datepickertest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.google.android.material.datepicker.MaterialDatePicker
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button_date.setOnClickListener {
+            showDatePicker()
+        }
+    }
+
+    private fun showDatePicker() {
         val builder = MaterialDatePicker.Builder.datePicker()
         val picker = builder.build()
         picker.show(supportFragmentManager, picker.toString())
@@ -21,10 +29,14 @@ class MainActivity : AppCompatActivity() {
         picker.addOnNegativeButtonClickListener {
             Log.d(TAG, "Dialog Negative Button was clicked")
         }
-
         picker.addOnPositiveButtonClickListener {
             Log.d(TAG, "Date String = ${picker.headerText}:: Date epoch value = ${it}")
+            showValue(picker.headerText)
         }
+    }
+
+    private fun showValue(value: String) {
+        Toast.makeText(this, value, Toast.LENGTH_SHORT).show()
     }
 
     companion object {
