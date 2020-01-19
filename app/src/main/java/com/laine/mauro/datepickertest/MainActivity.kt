a6add08103b47e4fd2fac1adfa0d1aa4d9d1dc9f
Mauricio Laine <laine.mau@gmail.com>
2020-01-19 15:06:00 -0600

    added listeners to check the different events in the picker

--- app/src/main/java/com/laine/mauro/datepickertest/MainActivity.kt
+++ app/src/main/java/com/laine/mauro/datepickertest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.laine.mauro.datepickertest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.datepicker.MaterialDatePicker
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,20 @@ class MainActivity : AppCompatActivity() {
         val builder = MaterialDatePicker.Builder.datePicker()
         val picker = builder.build()
         picker.show(supportFragmentManager, picker.toString())
+
+        picker.addOnCancelListener {
+            Log.d(TAG, "Dialog was cancelled")
+        }
+        picker.addOnNegativeButtonClickListener {
+            Log.d(TAG, "Dialog Negative Button was clicked")
+        }
+
+        picker.addOnPositiveButtonClickListener {
+            Log.d(TAG, "Date String = ${picker.headerText}:: Date epoch value = ${it}")
+        }
+    }
+
+    companion object {
+        val TAG = "DatePicker_Activity"
     }
 }
